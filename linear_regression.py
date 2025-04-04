d8a3a3b45c94b30b6a14545ed0df024b26637e31
Christianib003 <christianib003@gmail.com>
2025-04-04 18:59:07 +0200

    add plotting implementation of dataset

--- linear_regression.py
+++ linear_regression.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+data = pd.read_csv('data.csv')
+
+plt.scatter(data.studytime, data.score)
+plt.xlabel('Study Time')
+plt.ylabel('Score')
+plt.show()
